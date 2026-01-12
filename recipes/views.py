034d8920dc375050c9ee65e26456ad1151305f5c
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-01-12 17:18:04 -0300

    Atualização do sistema

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={'name':'Luiz Otavio'})
-def sobre(request):
-    return render(request, 'temp/git push origin maintemp.html')
-def contato(request):
-    return render(request, 'recipes/contato.html')
