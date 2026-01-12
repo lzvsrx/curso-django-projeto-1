89b2499691c1dfa3a6c761f6830782e8ff000f2b
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-01-12 18:08:57 -0300

    Atualização do sistema

--- recipes/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import render
-# Create your views here.
-def home(request):
-    return render(request, 'recipes/home.html', context={'name':'Luiz Otavio'})
