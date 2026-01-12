1a11f76005ea60066592088904a153f29e710091
lzvsrx <valenzisousaluizotavio@outlook.com>
2026-01-12 17:10:02 -0300

    Atualização do sistema

---
+++ recipes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from recipes.views import home, sobre, contato
+urlpatterns = [
+    path('', home, name='home'),
+    path('sobre/', sobre, name='sobre'),
+    path('contato/', contato, name='contato'),
+]
---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={'name':'Luiz Otavio'})
+def sobre(request):
+    return render(request, 'temp/temp.html')
+def contato(request):
+    return render(request, 'recipes/contato.html')
